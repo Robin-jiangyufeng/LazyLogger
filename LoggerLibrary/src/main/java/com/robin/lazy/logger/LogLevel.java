b2812aa610343812a207fb9413ade1f9f163587f
江钰锋 <429257411@qq.com>
2016-05-11 10:18:20 +0800

    修复了关闭日志输出无效的问题和修改版本号

--- LoggerLibrary/src/main/java/com/robin/lazy/logger/LogLevel.java
+++ LoggerLibrary/src/main/java/com/robin/lazy/logger/LogLevel.java
@@ -2,53 +2,53 @@ package com.robin.lazy.logger;
 
 public enum LogLevel {
 
-	/**
-	 * 输出所有类型日志
-	 */
-	ALL(0),
+    /**
+     * 输出所有类型日志
+     */
+    ALL(0),
 
-	/**
-	 * trace类型日志
-	 */
-	TRACE(1),
-	
-	/**
-	 * debug类型日志
-	 */
-	DEBUG(2),
+    /**
+     * trace类型日志
+     */
+    TRACE(1),
 
-	/**
-	 * info类型日志
-	 */
-	INFO(3),
-	
-	/**
-	 * warn类型日志
-	 */
-	WARN(4),
-	
-	/**
-	 * error类型日志
-	 */
-	ERROR(5),
-	
-	/**
-	 * fatal类型日志
-	 */
-	FATAL(6),
+    /**
+     * debug类型日志
+     */
+    DEBUG(2),
 
-	/**
-	 * 关闭日志输出
-	 */
-	OFF(7);
-	
-	private int code;
-	
-	private LogLevel(int code) {
+    /**
+     * info类型日志
+     */
+    INFO(3),
 
-	}
+    /**
+     * warn类型日志
+     */
+    WARN(4),
 
-	public int getCode() {
-		return code;
-	}
+    /**
+     * error类型日志
+     */
+    ERROR(5),
+
+    /**
+     * fatal类型日志
+     */
+    FATAL(6),
+
+    /**
+     * 关闭日志输出
+     */
+    OFF(7);
+
+    private int code;
+
+    private LogLevel(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
 }
